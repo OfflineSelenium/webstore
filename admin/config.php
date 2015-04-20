05c3be41069e849b6fa23bd6e177ddb1198a9d27
thinhnguyen100286 <ducthinh_100286@yahoo.com>
2015-04-21 01:38:52 +0700

    Fresh installed

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/webstore/admin/');
+define('HTTP_CATALOG', 'http://localhost/webstore/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/webstore/admin/');
+define('HTTPS_CATALOG', 'http://localhost/webstore/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\xampp\htdocs\webstore/admin/');
+define('DIR_SYSTEM', 'C:\xampp\htdocs\webstore/system/');
+define('DIR_DATABASE', 'C:\xampp\htdocs\webstore/system/database/');
+define('DIR_LANGUAGE', 'C:\xampp\htdocs\webstore/admin/language/');
+define('DIR_TEMPLATE', 'C:\xampp\htdocs\webstore/admin/view/template/');
+define('DIR_CONFIG', 'C:\xampp\htdocs\webstore/system/config/');
+define('DIR_IMAGE', 'C:\xampp\htdocs\webstore/image/');
+define('DIR_CACHE', 'C:\xampp\htdocs\webstore/system/cache/');
+define('DIR_DOWNLOAD', 'C:\xampp\htdocs\webstore/download/');
+define('DIR_LOGS', 'C:\xampp\htdocs\webstore/system/logs/');
+define('DIR_CATALOG', 'C:\xampp\htdocs\webstore/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'webstore');
+define('DB_PREFIX', '');
+?>
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/webstore/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/webstore/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\xampp\htdocs\webstore/catalog/');
+define('DIR_SYSTEM', 'C:\xampp\htdocs\webstore/system/');
+define('DIR_DATABASE', 'C:\xampp\htdocs\webstore/system/database/');
+define('DIR_LANGUAGE', 'C:\xampp\htdocs\webstore/catalog/language/');
+define('DIR_TEMPLATE', 'C:\xampp\htdocs\webstore/catalog/view/theme/');
+define('DIR_CONFIG', 'C:\xampp\htdocs\webstore/system/config/');
+define('DIR_IMAGE', 'C:\xampp\htdocs\webstore/image/');
+define('DIR_CACHE', 'C:\xampp\htdocs\webstore/system/cache/');
+define('DIR_DOWNLOAD', 'C:\xampp\htdocs\webstore/download/');
+define('DIR_LOGS', 'C:\xampp\htdocs\webstore/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'webstore');
+define('DB_PREFIX', '');
+?>
